7a1ced1a47612920ce362b41ba21d9027a839e29
Anthony Calcagno <z1829706@turing.cs.niu.edu>
2018-11-06 11:37:46 -0600

    Implemented bootstrap and got my form looking hawt.

--- create_band.php
+++ create_band.php
@@ -1,24 +1,41 @@
-<html>
-    <body>
-        <form action="" method="post">
-            <div>
+<?php include 'header.php'; ?>
+
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <h1>Create Band</h1>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-12">
+            <form action="" method="post">
+            <div class="form-group">
                 <label for="BandName">Band Name: </label>
-                <input type="text" id="BandName" placeholder="The Beatles">
+                <input class="form-control" type="text" id="BandName" placeholder="The Beatles">
             </div>
-            <div>
+            <div class="form-group">
                 <label for="BandRate">Rate per Event: </label>
-                <input type="text" id="BandRate" placeholder="100">
+                <input class="form-control" type="text" id="BandRate" placeholder="100">
             </div>
-            <div>
+            <div class="form-group">
                 <label for="BandAgent">Band Agent: </label>
-                <input type="text" id="Band Agent" placeholder="Jim Agent">
+                <input class="form-control" type="text" id="Band Agent" placeholder="Jim Agent">
             </div>
-            <div>
+            <div class="form-group">
                 <label for="BandPhone">Phone Number for Band: </label>
-                <input type="text" id="BandPhone" placeholder="(630)555-5555">
+                <input class="form-control" type="text" id="BandPhone" placeholder="(630)555-5555">
             </div>
-             <div>
+            <div class="form-group">
                 <label for="Notes">Notes: </label>
-                <input type="text" id="Notes">
-    </body>
-</html>
+                <input class="form-control" type="text" id="Notes">
+            </div>
+            <div class="form-group">
+                <button type="submit" class="btn btn-success">Create</button>
+                <button type="submit" class="btn btn-danger">Cancel</button>
+            </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+<?php include 'footer.php'; ?>
--- header.php
+++ header.php
@@ -1,1 +1,23 @@
-header
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+  </head>
+
+
+<?php
+    try {
+        $dsn = "mysql:host=courses;dbname=z1829706";
+        $username = "z1829706";
+        $password = "1995Sep13";
+        $pdo = new PDO($dsn, $username, $password);
+        }
+    catch(PDOexception $e) {
+        echo "Connection to database failed: " . $e->getMessage();
+        }
+
+?>
+
+  <body>
