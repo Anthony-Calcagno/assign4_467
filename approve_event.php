2c7256e200f447c249e16dff24bf40d696a2e42b
Anthony Calcagno <z1829706@turing.cs.niu.edu>
2018-11-06 14:46:20 -0600

    Actually added approve event

---
+++ approve_event.php
@@ -0,0 +1,47 @@
+<?php include 'header.php'; ?>
+
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <h1>Approve Event</h1>
+        </div>
+    </div>
+</div>
+
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <table class="table">
+                <thead class="thead-dark">
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Event</th>
+                        <th scope="col">Info</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>placeholder</td>
+                        <td>placeholder</td>
+                        <td>placeholder</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <form method="post" action="">
+                <div class="form-group">
+                    <button type="submit" class="btn btn-danger">Cancel</button>
+                    <button type="submit" class="btn btn-success">Approve</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+<?php include 'footer.php'; ?>
