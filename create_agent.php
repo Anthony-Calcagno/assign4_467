28cedf97157980bb69f3d59ae4957ce13f256468
Anthony Calcagno <z1829706@turing.cs.niu.edu>
2018-11-06 17:46:28 -0600

    got some php working

--- create_agent.php
+++ create_agent.php
@@ -1,5 +1,30 @@
 <?php include 'header.php'; ?>
 
+<?php
+
+if($_SERVER['REQUEST_METHOD'] == 'POST')
+{  
+  $Fname = trim($_POST['Fname']);
+  $Minit = trim($_POST['Minit']);
+  $Lname = trim($_POST['Lname']);
+  $AgentEmail = trim($_POST['Email']);
+  $OfficePhone = trim($_POST['OfficePhone']);
+  $CellPhone = trim($_POST['CellPhone']);
+  $AgentType = trim($_POST['Type']);
+  $AgentStreet = trim($_POST['Street']);
+  $AgentCity = trim($_POST['City']);
+  $AgentState = trim($_POST['State']);
+  $AgentZip = trim($_POST['Zip']);
+
+  $sql = "INSERT INTO Agent (Fname, Minit, Lname, AgentEmail, OfficePhone, CellPhone, AgentType, AgentStreet, AgentCity, AgentState, AgentZip) VALUES (:Fname, :Minit, :Lname, :AgentEmail, :OfficePhone, :CellPhone, :AgentType, :AgentStreet, :AgentCity, :AgentState, :AgentZip) ";
+  $prepared = $pdo->prepare($sql, array(PDO::ATTR_CURSOR =>PDO::CURSOR_FWDONLY));
+  $result = $prepared->execute(array(':Fname' => $Fname, ':Minit' => $Minit, ':Lname' => $Lname, ':AgentEmail' => $AgentEmail, ':OfficePhone' => $OfficePhone, ':CellPhone' => $CellPhone, ':AgentType' => $AgentType, ':AgentStreet' => $AgentStreet, ':AgentCity' => $AgentCity, ':AgentState' => $AgentState, ':AgentZip' => $AgentZip));
+}
+
+?>
+
+
+
 <div class="container">
     <div class="row">
         <div class="col-10">
@@ -11,31 +36,31 @@
         <form action="" method="post">
             <div class="form-group">
                 <label for="Fname">First Name: </label>
-                <input type="text" id="Fname" class="form-control">
+                <input type="text" name="Fname" class="form-control">
             </div>
             <div class="form-group">
-                <label for="Minit">Middle Initial: </label>
-                <input type="text" id="Minit" class="form-control">
+                <label for="Minit">Mnamedle Initial: </label>
+                <input type="text" name="Minit" class="form-control">
             </div>
             <div class="form-group">
                 <label for="Lname">Last Name: </label>
-                <input type="text" id="Lname" class="form-control">
+                <input type="text" name="Lname" class="form-control">
             </div>
             <div class="form-group">
                 <label for="Email">Email Address: </label>
-                <input type="email" id="Email" class="form-control">
+                <input type="email" name="Email" class="form-control">
             </div>
             <div class="form-group">
                 <label for="OfficePhone">Office Phone Number: </label>
-                <input type="text" id="OfficePhone" class="form-control">
+                <input type="text" name="OfficePhone" class="form-control">
             </div>
             <div class="form-group">
                 <label for="CellPhone">Cell Phone Number: </label>
-                <input type="text" id="CellPhone" class="form-control">
+                <input type="text" name="CellPhone" class="form-control">
             </div>
             <div class="form-group">
                 <label>Type: </label>
-                <select name="Type" class="form-control" >
+                <select name="Type" class="form-control">
                     <option value="Artist">Artist</option>
                     <option value="Band">Band</option>
                     <option value="Other">Other</option>
@@ -43,19 +68,19 @@
             </div>
             <div class="form-group">
                 <label for="Street">Street Address: </label>
-                <input type="text" id="Street" class="form-control">
+                <input type="text" name="Street" class="form-control">
             </div>
             <div class="form-group">
                 <label for="City">City: </label>
-                <input type="text" id="City" class="form-control">
+                <input type="text" name="City" class="form-control">
             </div>
             <div class="form-group">
                 <label for="State">State: </label>
-                <input type="text" id="State" class="form-control">
+                <input type="text" name="State" class="form-control">
             </div>
             <div class="form-group">
                 <label for="Zip">Zip Code: </label>
-                <input type="text" id="Zip" class="form-control">
+                <input type="text" name="Zip" class="form-control">
             </div>
             <div class="form-group">
                 <button type="submit" class="btn btn-success">Create</button>
--- header.php
+++ header.php
@@ -13,6 +13,8 @@
         $username = "z1829706";
         $password = "1995Sep13";
         $pdo = new PDO($dsn, $username, $password);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
         }
     catch(PDOexception $e) {
         echo "Connection to database failed: " . $e->getMessage();
